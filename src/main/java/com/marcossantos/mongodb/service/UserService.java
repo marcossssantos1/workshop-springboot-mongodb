9697aab3ebcf845125187b51b8a47351516d07b3
Marcos Santos <marcossantosmsms604@gmail.com>
2024-02-22 20:30:19 -0300

    Obtendo um usuario por id

--- src/main/java/com/marcossantos/mongodb/service/UserService.java
+++ src/main/java/com/marcossantos/mongodb/service/UserService.java
@@ -1,12 +1,14 @@
 package com.marcossantos.mongodb.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcossantos.mongodb.domain.User;
 import com.marcossantos.mongodb.repository.UserRepository;
+import com.marcossantos.mongodb.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
 	public List <User> findAll() {
          return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
