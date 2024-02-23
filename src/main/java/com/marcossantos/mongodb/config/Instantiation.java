94318d729a0008aaf047a19a4db1df08f20725b8
Marcos Santos <marcossantosmsms604@gmail.com>
2024-02-23 15:29:29 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/marcossantos/mongodb/config/Instantiation.java
+++ src/main/java/com/marcossantos/mongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import com.marcossantos.mongodb.domain.Post;
 import com.marcossantos.mongodb.domain.User;
 import com.marcossantos.mongodb.dto.AuthorDTO;
+import com.marcossantos.mongodb.dto.CommentDTO;
 import com.marcossantos.mongodb.repository.PostRepository;
 import com.marcossantos.mongodb.repository.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
 				new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/marcossantos/mongodb/domain/Post.java
+++ src/main/java/com/marcossantos/mongodb/domain/Post.java
@@ -1,25 +1,30 @@
 package com.marcossantos.mongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.marcossantos.mongodb.dto.AuthorDTO;
+import com.marcossantos.mongodb.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
+
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 		// TODO Auto-generated constructor stub
@@ -72,6 +77,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
