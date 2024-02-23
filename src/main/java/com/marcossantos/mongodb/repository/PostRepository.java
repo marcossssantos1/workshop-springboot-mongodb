a5e6f7043968a44b7d010fc3489e9793dbf3a4e9
Marcos Santos <marcossantosmsms604@gmail.com>
2024-02-23 16:07:58 -0300

    Consulta simples com @query

--- src/main/java/com/marcossantos/mongodb/repository/PostRepository.java
+++ src/main/java/com/marcossantos/mongodb/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.marcossantos.mongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.marcossantos.mongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
